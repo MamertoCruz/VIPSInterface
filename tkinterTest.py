8205a38b64cf27dbf3f9ad39eae67874630146f8
MamertoCruz <mamerto.cruz3@gmail.com>
2023-02-28 15:29:56 -0600

    gui test

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,19 +1,46 @@
 from tkinter import *
+import ImageTk
 
 gui = Tk()
 
-gui.geometry("500x200")
+gui.geometry("600x400")
+gui.title("Test")
 
-bg= PhotoImage(file = "VIPS_Logo.png")
+frame = Frame(gui)
+frame.pack()
 
-label1= Label(gui, image = bg)
-label1.place(x = 0, y = 0)
+canvas = Canvas(frame, bg="black", width=500, height=500)
+canvas.pack()
 
-label2 = Label(gui, text = "Welcome", bg = "#88cffa")
-label2.pack(pady = 50)
+photoimage = ImageTk.PhotoImage(file="VIP_Logo.png")
+canvas.create_image(600, 400, image=photoimage)
 
-frame1 = Frame(gui, bg = "#88cffa")
-frame1.pack(pady = 20)
+#t.mainloop()
+
+#ibg= PhotoImage(file = "VIPS_Logo.png")
+
+#canvas1 = Canvas( gui, width = 400,
+#                 height = 400)
+  
+#canvas1.pack(fill = "both", expand = True)
+  
+# Display image
+#canvas1.create_image( 0, 0, image = ibg, 
+                     anchor = "nw")
+  
+#gui['bg']='green'
+
+# Add Text
+#canvas1.create_text( 200, 250, text = "Welcome")
+  
+#label1= Label(gui, image = bg)
+#label1.place(x = 0, y = 0)
+
+#label2 = Label(gui, text = "Welcome", bg = "#88cffa")
+#label2.pack(pady = 50)
+
+#frame1 = Frame(gui, bg = "#88cffa")
+#frame1.pack(pady = 20)
 
 gui.mainloop()
 
